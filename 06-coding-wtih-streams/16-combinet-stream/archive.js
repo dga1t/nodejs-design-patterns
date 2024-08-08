8772e6bd3b2e7c559b192748cd9669f8f4cba4ec
Dima Pasumansky <dpsmnsk@gmail.com>
2024-08-08 08:35:10 +0500

    chapter 6 wip

---
+++ 06-coding-wtih-streams/16-combinet-stream/archive.js
@@ -0,0 +1,22 @@
+import { createReadStream, createWriteStream } from 'node:fs'
+import { pipeline } from 'node:stream'
+import { randomBytes } from 'node:crypto'
+
+import { createCompressAndEncrypt } from './combined-streams.js'
+
+const [,, password, source] = process.argv
+const iv = randomBytes(16)
+const destination = `${source}.gz.enc`
+
+pipeline(
+  createReadStream(source),
+  createCompressAndEncrypt(password, iv),
+  createWriteStream(destination),
+  (err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    console.log(`${destination} created with iv: ${iv.toString('hex')}`)
+  }
+)
