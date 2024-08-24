acdd11f0fd6f69b42ccc5c23859bbdd0b446c2a2
Dima Pasumansky <dpsmnsk@gmail.com>
2024-08-24 22:04:27 +0500

    chapter 7 WIP

---
+++ 07-creational-design-patterns/06-dependency-injection/index.js
@@ -0,0 +1,33 @@
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+import { createDb } from './db.js'
+import { Blog } from './blog.js'
+
+// This example shows how to wire dependencies using the dependency injection pattern
+
+// Usage:
+// node import-posts.js
+// node index.js
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+async function main () {
+  const db = createDb(join(__dirname, 'data.sqlite'))
+  const blog = new Blog(db)
+  
+  await blog.initialize()
+  const posts = await blog.getAllPosts()
+  
+  if (posts.length === 0) {
+    console.log('No post available. Run `node import-posts.js` to load some sample posts')
+  }
+
+  for (const post of posts) {
+    console.log(post.title)
+    console.log('-'.repeat(post.title.length))
+    console.log(`Published on ${new Date(post.created_at).toISOString()}`)
+    console.log(post.content)
+  }
+}
+
+main().catch(console.error)
